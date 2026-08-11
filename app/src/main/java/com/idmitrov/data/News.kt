5adf90b6fb55cceb5a5bb365f9cd3fffbdbd3eaf
gfgydy35-dotcom <gfgydy35@yandex.ru>
2026-08-11 10:51:14 +0300

    Create News.kt

---
+++ app/src/main/java/com/idmitrov/data/News.kt
@@ -0,0 +1,9 @@
+package com.idmitrov.data
+
+data class News(
+    val id: Int = 0,
+    val title: String = "",
+    val content: String = "",
+    val image_url: String = "",
+    val created_at: String = ""
+)
